afdf97ae42b23835c1491409b10481aa013b19f0
Anti049 <nploetz049@gmail.com>
2024-05-02 09:54:59 -0500

    Added support for shadcn-svelte and tauri

--- src/components/app_shell/app_shell.svelte
+++ src/components/app_shell/app_shell.svelte
@@ -1,3 +1,17 @@
+<!-- 
+	@component
+	## AppShell
+	/**
+	 * @slot appHeader - Insert fixed header content, such as an app bar.
+	 * @slot sidebarLeft - Insert content for the left sidebar. Hidden when empty.
+	 * @slot sidebarRight - Insert content for the right sidebar. Hidden when empty.
+	 * @slot pageHeader - Insert content that resides above your page content. Good for global alerts.
+	 * @slot pageContent - Insert your main page content here.
+	 * @slot pageFooter - Insert content that resides below your page content. Recommended for most layouts.
+	 * @slot appFooter - Insert fixed footer content. Not recommended for most layouts.
+	 */
+	
+ -->
 <script lang="ts">
 	// Slots
 	/**
@@ -5,8 +19,8 @@
 	 * @slot sidebarLeft - Insert content for the left sidebar. Hidden when empty.
 	 * @slot sidebarRight - Insert content for the right sidebar. Hidden when empty.
 	 * @slot pageHeader - Insert content that resides above your page content. Good for global alerts.
-	 * @slot pageFooter - Insert content that resides below your page content. Recommended for most layouts.
 	 * @slot pageContent - Insert your main page content here.
+	 * @slot pageFooter - Insert content that resides below your page content. Recommended for most layouts.
 	 * @slot appFooter - Insert fixed footer content. Not recommended for most layouts.
 	 */
 
@@ -14,14 +28,43 @@
 	import type { CssClasses, SvelteEvent } from '@/utils';
 
 	// Props
+	/** Set 'scrollbar-gutter' style. */
+	export let scrollbarGutter = 'auto';
+
+	// Props (Regions)
+	/** Apply arbitrary classes to the entire '#page' region. */
 	export let regionPageClasses: CssClasses = '';
+	/** Apply arbitrary classes to the 'appHeader' slot container element. */
+	export let slotAppHeaderClasses: CssClasses = 'z-10';
+	/** Apply arbitrary classes to the 'sidebarLeft' slot container element. */
+	export let slotSidebarLeftClasses: CssClasses = 'w-auto';
+	/** Apply arbitrary classes to the 'sidebarRight' slot container element. */
+	export let slotSidebarRightClasses: CssClasses = 'w-auto';
+	/** Apply arbitrary classes to the 'pageHeader' slot container element. */
+	export let slotPageHeaderClasses: CssClasses = '';
+	/** Apply arbitrary classes to the 'pageContent' slot container element. */
+	export let slotPageContentClasses: CssClasses = '';
+	/** Apply arbitrary classes to the 'pageFooter' slot container element. */
+	export let slotPageFooterClasses: CssClasses = '';
+	/** Apply arbitrary classes to the 'appFooter' slot container element. */
+	export let slotAppFooterClasses: CssClasses = '';
 
 	// Base classes
 	const cAppShellClasses = 'w-full h-full flex flex-col overflow-hidden';
+	const cContentClasses = 'w-full h-full flex overflow-hidden';
+	const cPageClasses = 'flex flex-1 flex-col overflow-x-hidden';
+	const cSidebarLeftClasses = 'flex-none overflow-x-hidden overflow-y-auto';
+	const cSidebarRightClasses = 'flex-none overflow-x-hidden overflow-y-auto';
 
 	// Reactive classes
 	$: appShellClasses = `${cAppShellClasses} ${$$props.class ?? ''}`;
-	$: appHeaderClasses = '';
+	$: appHeaderClasses = `${slotAppHeaderClasses}`;
+	$: sidebarLeftClasses = `${cSidebarLeftClasses} ${slotSidebarLeftClasses}`;
+	$: sidebarRightClasses = `${cSidebarRightClasses} ${slotSidebarRightClasses}`;
+	$: pageHeaderClasses = `${slotPageHeaderClasses}`;
+	$: pageContentClasses = `${slotPageContentClasses}`;
+	$: pageFooterClasses = `${slotPageFooterClasses}`;
+	$: appFooterClasses = `${slotAppFooterClasses}`;
 </script>
 
 <div id="appShell" class={appShellClasses} data-testid="app-shell">
@@ -30,5 +73,50 @@
 		<header id="shell-header" class="flex-none {appHeaderClasses}">
 			<slot name="appHeader" />
 		</header>
+	{/if}
+	<!-- Content Area -->
+	<div class="flex-auto {cContentClasses}">
+		<!-- Slot: Sidebar (Left) -->
+		{#if $$slots.sidebarLeft}
+			<aside id="shell-sidebar-left" class={sidebarLeftClasses}>
+				<slot name="sidebarLeft" />
+			</aside>
+		{/if}
+		<!-- Page -->
+		<div
+			id="page"
+			class="{regionPageClasses} {cPageClasses}"
+			style:scrollbar-gutter={scrollbarGutter}
+			on:scroll
+		>
+			<!-- Slot: Page Header -->
+			{#if $$slots.pageHeader}
+				<header id="shell-page-header" class="flex-none {pageHeaderClasses}">
+					<slot name="pageHeader">(slot:header)</slot>
+				</header>
+			{/if}
+			<!-- Slot: Page Content (Default) -->
+			<main id="shell-page-content" class="flex-auto {pageContentClasses}">
+				<slot />
+			</main>
+			<!-- Slot: Page Footer -->
+			{#if $$slots.pageFooter}
+				<footer id="shell-page-footer" class="flex-none {pageFooterClasses}">
+					<slot name="pageFooter" />
+				</footer>
+			{/if}
+		</div>
+		<!-- Slot: Sidebar (Right) -->
+		{#if $$slots.sidebarRight}
+			<aside id="shell-sidebar-right" class={sidebarRightClasses}>
+				<slot name="sidebarRight" />
+			</aside>
+		{/if}
+	</div>
+	<!-- Slot: Footer -->
+	{#if $$slots.appFooter}
+		<footer id="shell-footer" class="flex-none {appFooterClasses}">
+			<slot name="appFooter" />
+		</footer>
 	{/if}
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,125 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+	import Separator from '@/components/ui/separator/separator.svelte';
+	import '../app.pcss';
+	import AppShell from '@/components/app_shell/app_shell.svelte';
+	import Switch from '@/components/ui/switch/switch.svelte';
+	import Label from '@/components/ui/label/label.svelte';
+	import Button from '@/components/ui/button/button.svelte';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
+	import { Sun, Moon, Bell } from 'lucide-svelte';
+	import { Toaster } from '@/components/ui/sonner';
+	import { toast } from 'svelte-sonner';
+
+	let showAppHeader = true;
+	let showAppFooter = true;
+	let showPageHeader = true;
+	let showPageFooter = true;
+	let showSidebarLeft = true;
+	let showSidebarRight = true;
+</script>
+
+<ModeWatcher />
+<Toaster richColors />
+<AppShell class="text-black dark:text-white">
+	<!-- App Header -->
+	<svelte:fragment slot="appHeader">
+		{#if showAppHeader}
+			<div class="flex h-16 flex-row items-center gap-2 bg-red-200 p-4 dark:bg-red-800">
+				<h1 class="w-full text-white">App Header</h1>
+				<Button
+					on:click={() =>
+						toast.success('This is a toast message', {
+							description: 'Test Message',
+							icon: Bell,
+							action: { label: 'Undo', onClick: () => console.info('Undo') }
+						})}
+					variant="outline"
+					size="icon"
+				>
+					<Bell class="h-[1.2rem] w-[1.2rem]" />
+					<span class="sr-only">Show notifications</span>
+				</Button>
+				<Button on:click={toggleMode} variant="outline" size="icon">
+					<Sun
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Moon
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</div>
+		{/if}
+	</svelte:fragment>
+	<!-- App Footer -->
+	<svelte:fragment slot="appFooter">
+		{#if showAppFooter}
+			<div class="h-16 bg-blue-500">
+				<h1 class="text-white">App Footer</h1>
+			</div>
+		{/if}
+	</svelte:fragment>
+	<!-- Page Header -->
+	<svelte:fragment slot="pageHeader">
+		{#if showPageHeader}
+			<div class="h-16 bg-green-500">
+				<h1 class="text-white">Page Header</h1>
+			</div>
+		{/if}
+	</svelte:fragment>
+	<!-- Page Footer -->
+	<svelte:fragment slot="pageFooter">
+		{#if showPageFooter}
+			<div class="h-16 bg-yellow-500">
+				<h1 class="text-white">Page Footer</h1>
+			</div>
+		{/if}
+	</svelte:fragment>
+	<!-- Sidebar (Left) -->
+	<svelte:fragment slot="sidebarLeft">
+		{#if showSidebarLeft}
+			<div class="h-full w-64 bg-gray-500">
+				<h1 class="text-white">Sidebar (Left)</h1>
+			</div>
+		{/if}
+	</svelte:fragment>
+	<!-- Sidebar (Right) -->
+	<svelte:fragment slot="sidebarRight">
+		{#if showSidebarRight}
+			<div class="h-full w-64 bg-gray-500">
+				<h1 class="text-white">Sidebar (Right)</h1>
+			</div>
+		{/if}
+	</svelte:fragment>
+	<!-- Page Content -->
+	<div class="h-full w-full bg-white p-2 text-black dark:bg-black dark:text-white">
+		<div class="flex flex-col gap-2">
+			<div class="flex h-full flex-row items-center gap-2">
+				<Switch id="show-app-header" bind:checked={showAppHeader} />
+				<Label for="show-app-header">Show App Header</Label>
+			</div>
+			<div class="flex h-full flex-row items-center gap-2 align-middle">
+				<Switch id="show-app-footer" bind:checked={showAppFooter} />
+				<Label for="show-app-footer">Show App Footer</Label>
+			</div>
+			<div class="flex h-full flex-row items-center gap-2 align-middle">
+				<Switch id="show-page-header" bind:checked={showPageHeader} />
+				<Label for="show-page-header">Show Page Header</Label>
+			</div>
+			<div class="flex h-full flex-row items-center gap-2 align-middle">
+				<Switch id="show-page-footer" bind:checked={showPageFooter} />
+				<Label for="show-page-footer">Show Page Footer</Label>
+			</div>
+			<div class="flex h-full flex-row items-center gap-2 align-middle">
+				<Switch id="show-sidebar-left" bind:checked={showSidebarLeft} />
+				<Label for="show-sidebar-left">Show Sidebar (Left)</Label>
+			</div>
+			<div class="flex h-full flex-row items-center gap-2 align-middle">
+				<Switch id="show-sidebar-right" bind:checked={showSidebarRight} />
+				<Label for="show-sidebar-right">Show Sidebar (Right)</Label>
+			</div>
+		</div>
+		<Separator class="my-4" />
+		<slot />
+	</div>
+</AppShell>
